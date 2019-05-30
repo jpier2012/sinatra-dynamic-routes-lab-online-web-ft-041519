db28df906fc0f3fbe17e751ce5a4fa86d0207cb7
James Pier <jpier2012@gmail.com>
2019-05-30 00:28:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,10 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number]
     @phrase = params[:phrase].gsub(' ', "%20")
-
-    @number.to_i.times do
-      "#{@phrase}"
-    end
+    "#{@phrase}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
