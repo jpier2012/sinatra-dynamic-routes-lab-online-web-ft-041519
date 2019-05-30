627bc3022add59db579adfabf50418e4114e98e3
James Pier <jpier2012@gmail.com>
2019-05-30 00:32:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,6 @@ class App < Sinatra::Base
     @number = params[:number]
     @phrase = params[:phrase].gsub('%20', ' ')
     Array.new(@number, @phrase).each { |p| puts "#{@phrase}" }
-    
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
